fbcf17d13940a50146f5fc8c9825929b9cc5ec38
Michał Kątnik <michal.katnik91@gmail.com>
2023-03-11 23:04:07 +0100

    cleanup

--- Tests/estheticshTests/estheticshTests.swift
+++ Tests/estheticshTests/estheticshTests.swift
@@ -2,10 +2,6 @@ import XCTest
 @testable import estheticsh
 
 final class estheticshTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(estheticsh().text, "Hello, World!")
-    }
+    
+
 }
