66417e6af32c2663bee8e12640510aea6c816fba
Michał Kątnik <michal.katnik91@gmail.com>
2023-03-12 01:53:01 +0100

    printAllColors() added.

--- Sources/estheticsh/estheticsh.swift
+++ Sources/estheticsh/estheticsh.swift
@@ -32,9 +32,9 @@ struct Estheticsh: ParsableCommand {
     var bgColor: String? = nil
    
     // Flags.
-    @Flag(name: .long, help: "Show all available color names.")
+    @Flag(name: .long, help: "Print all available color names.")
     var colors = false
-    @Flag(name: .long, help: "Show usage examples.")
+    @Flag(name: .long, help: "Print usage examples.")
     var examples = false
     @Flag(name: .long, help: "Print the original name of this application.")
     var name = false
@@ -51,6 +51,7 @@ struct Estheticsh: ParsableCommand {
         if name { print(PkgInfo.name); Estheticsh.exit() }
         if author { print(PkgInfo.author); Estheticsh.exit() }
         if repository { print(PkgInfo.repository); Estheticsh.exit() }
+        if colors { printAllColors(); Estheticsh.exit() }
 
         
         // Exit app with error, if the "text" argument is not given.
@@ -62,4 +63,61 @@ struct Estheticsh: ParsableCommand {
         }
     }
     
+    
+    // Print all available color names.
+    func printAllColors() {
+        let minLength = 20
+        let header = "Name:" + String(repeating: " ", count: minLength - 5) + "Option:"
+        let marginLeft = String(repeating: " ", count: 2)
+        var colorNameLength = 0
+        var colorNameString = ""
+             
+        // Background colors.
+        print("\n---- Background Colors ----\n\(header)")
+        for b in BackgroundColor.allCases {
+            colorNameLength = "\(b)".count
+            colorNameString = marginLeft + "\(b)"
+            colorNameString += String(
+                repeating: " ",
+                count: minLength - colorNameLength - marginLeft.count
+            )
+            
+            // Change the foreground color for better visibility in some cases.
+            if  b != BackgroundColor.black &&
+                b != BackgroundColor.default &&
+                b != BackgroundColor.lightBlack
+            {
+                colorNameString = colorNameString.applyingColor(Color.black)
+            }
+            
+            print(
+                colorNameString.applyingBackgroundColor(b) +
+                marginLeft + "-b \(b)"
+            )
+        }
+        
+        // Foreground colors.
+        print("\n---- Foreground Colors ----\n\(header)")
+        for c in Color.allCases {
+            colorNameLength = "\(c)".count
+            colorNameString = "\(c)"
+            
+            // If fg color is black, change background color for better visibility.
+            if c == Color.black {
+                colorNameString = colorNameString
+                    .applyingBackgroundColor(BackgroundColor.lightBlack)
+            }
+            
+            colorNameString = marginLeft + colorNameString
+            colorNameString += String(repeating: " ", count: minLength - colorNameLength)
+            
+            print(colorNameString.applyingColor(c) + "-c \(c)")
+        }
+        
+        // Blank line.
+        print()
+    }
+    
 }
+
+
